11b56497120e2d674e93ce52dac1e9a46de84086
Kononiuk <hmasteraaa@gmail.com>
2025-01-10 19:31:58 +0200

    init

---
+++ app/page.js
@@ -0,0 +1,14 @@
+'use client';
+import Image from 'next/image';
+import { useState, useEffect, useRef } from 'react';
+import ChatComponent from './ChatComponent';
+
+export default function Home() {
+   return (
+      <div className=''>
+         <div className='flex justify-between px-4 border border-[]'>
+            
+         </div>
+      </div>
+   );
+}
